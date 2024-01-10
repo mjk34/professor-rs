65dd68d1e0f6d81c16076d72eba971850145cf56
MK-C0de <mjk@cs-code.support>
2024-01-10 16:19:19 -0500

    test gitignore

---
+++ README.md
@@ -0,0 +1,17 @@
+# professorBot
+
+## Summary:
+This is the code behind Uwuversity's Professor bot.
+
+Professor bot is a developing agent that is used to incentivise members with rewarding and interactive feature to improve participation. Members are currently able to accumulate creds daily, sumbit game clips for Clip Night, and buy tickets to participate in the ongoing raffles.
+
+
+## Install
+
+---
+
+## Current Development Branches:
+  - [ ] main
+
+## Past Development Branches:
+  - [-] main     # transition from python to rust
---
+++ professor-rs/src/main.rs
@@ -0,0 +1,61 @@
+use poise::serenity_prelude as serenity;
+use serenity::{ChannelId, MessageId};
+use tokio::sync::Mutex;
+
+use std::{collections::HashMap, env::var, time::Duration};
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+pub struct Data {
+    usernames: Mutex<HashMap<u64, (String, String)>>,
+}
+
+// Show this help menu
+#[poise::command(prefix_command, track_edits, slash_command)]
+async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"]
+    #[autocomplete = "poise::builtins::autocomplete_command"]
+    command: Option<String>,
+) -> Result<(), Error> {
+    poise::builtins::help(
+        ctx,
+        command.as_deref(),
+        poise::builtins::HelpConfiguration {
+            extra_text_at_bottom: "\\Valorant Bot",
+            show_context_menu_commands: true,
+            ..Default::default()
+        },
+    )
+    .await?;
+    Ok(())
+}
+
+#[poise::command(prefix_command, hide_in_help)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
